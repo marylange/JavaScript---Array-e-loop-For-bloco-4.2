3ab6ab7fa487dc3d2a04a979b0c17ef3f030046d
Marylange <marylangess@gmail.com>
2020-05-14 21:00:41 -0300

    atualizando

--- exercise_6.js
+++ exercise_6.js
@@ -1,24 +1,18 @@
-let numbers = [5, 9, 3, 19, 35, 27];
+let numbers = [1, 3, 5, 7, 2, 4, 6, 8,];
 
-// let result = 0;
-// for(let i = 0; i < numbers.length; i += 1) {
+let result = [];
+for(let i = 0; i < numbers.length; i += 1) {
+  if(numbers[i] % 2 !== 0) {
+    result.push(numbers[i]);
+  }
+}
+if(result.length === 0) {
+	console.log('nenhum valor ímpar encontrado');
+} else {
+	console.log(result);
+}
 
-// 	if(numbers[i] % 2 !== 0) {
-// 		result += 1;
-// 		console.log(result);
-// 	}
-
-// 	console.log('nenhum valor ímpar encontrado');
-// }
-let a = [];
-const c = numbers.filter((element) => {
-	let result = 0;
-	if(element % 2 !== 0){
-		result += 1;
-		a.push(result);
-		console.log(a);
-	};
-	return 'nenhum valor ímpar encontrado';
-});
-
-console.log(c);
+/*
+Descubra quantos valores ímpares existem no array e imprima o resultado. 
+Caso não exista nenhum, imprima a mensagem: “nenhum valor ímpar encontrado”;
+*/
